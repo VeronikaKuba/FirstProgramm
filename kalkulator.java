f3024b641aaa6962395253af3754672fee51d33f
VeronikaKuba <nikahappycat@gmail.com>
2022-03-19 12:17:03 +0100

    hy this is calculator.

---
+++ kalkulator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class kalkulator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println("Введіть дюйм");
+ //       double inches = scanner.nextDouble();
+ //       double meters = inches * 0.0254;
+     //   System.out.println(meters);
+           ///
+//        float a =scanner.nextInt();
+  //      System.out.println(a + "число" );
+  //      System.out.println("ВВедіть наступне число_");
+  //      float b = scanner.nextInt();
+ //       float c = a / b;
+ //       System.out.println("Відповідь: " + c);
+
+    }
+}
