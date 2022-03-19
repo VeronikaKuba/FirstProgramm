a8b5074661f3a8636e7a305984b76d6495bda2b7
Veronika Kuba <nikahappycat@gmail.com>
2022-03-19 12:27:44 +0100

    hy this is calculator.

--- kalkulator.java
+++ kalkulator.java
@@ -8,12 +8,12 @@ public class kalkulator {
  //       double meters = inches * 0.0254;
      //   System.out.println(meters);
            ///
-//        float a =scanner.nextInt();
-  //      System.out.println(a + "число" );
-  //      System.out.println("ВВедіть наступне число_");
-  //      float b = scanner.nextInt();
- //       float c = a / b;
- //       System.out.println("Відповідь: " + c);
+        float a =scanner.nextInt();
+       System.out.println(a + "число" );
+       System.out.println("ВВедіть наступне число_");
+        float b = scanner.nextInt();
+       float c = a / b;
+       System.out.println("Відповідь: " + c);
 
     }
 }
